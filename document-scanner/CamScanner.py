c0ab595b20345a8566b7666b009c2245027a564b
Manab Chetia <manab.chetia@outlook.com>
2015-02-04 13:21:44 +1100

    CamScanner completed

--- document-scanner/CamScanner.py
+++ document-scanner/CamScanner.py
@@ -1,14 +1,36 @@
 __author__ = 'manabchetia'
 
 from pyimagesearch import imutils
+from pyimagesearch.transform import four_point_transform
+from skimage.filter import threshold_adaptive
 import cv2
 
 
 def resize(image):
-    height = 500
-    ratio = image.shape[0] / height
-    image = imutils.resize(image, height)
+    ratio = image.shape[0] / 500.0
+    image = imutils.resize(image, height=500)
     return image, ratio
+
+
+def get_contours(edge_image):
+    contours, _ = cv2.findContours(edge_image.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    return contours
+
+
+def get_4_corners(edge_image):
+    contours = get_contours(edge_image)
+    for contour in contours:
+        epsilon = 0.02 * cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, epsilon, True)
+
+        if len(approx) == 4:
+            corners_4 = approx
+            break
+
+    page = cv2.convexHull(corners_4)
+
+    return page
 
 
 def main():
@@ -24,10 +46,20 @@ def main():
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edge_image = cv2.Canny(gray_image, 75, 200)
 
-    # Show Image
-    cv2.imshow("Image", image)
-    cv2.imshow("Edge Image", edge_image)
+    # Get 4 corners of page
+    page = get_4_corners(edge_image)
+    cv2.drawContours(image, [page], -1,  (0, 255, 0), 2)
+
+    # Get top-down view of original image using 4 point transform
+    top_view_image = four_point_transform(orig_image, page.reshape(4, 2) * ratio)
+    top_view_image = cv2.cvtColor(top_view_image, cv2.COLOR_BGR2GRAY)
+    top_view_image = threshold_adaptive(top_view_image, 255, offset=10)
+    scanned_image  = top_view_image.astype("uint8") * 255
+
+
+    cv2.imshow("Scanned image", imutils.resize(scanned_image, height=650))
     cv2.waitKey(0)
+
 
 
 if __name__ == '__main__': main()
--- document-scanner/scan.py
+++ document-scanner/scan.py
@@ -2,25 +2,30 @@
 # python scan.py --image images/page.jpg 
 
 # import the necessary packages
-from pyimagesearch.transform import four_point_transform
-from pyimagesearch import imutils
-from skimage.filter import threshold_adaptive
-import numpy as np
 import argparse
+
+from skimage.filter import threshold_adaptive
 import cv2
 
+from pyimagesearch.transform import four_point_transform
+from pyimagesearch import imutils
+
+
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True,
-	help = "Path to the image to be scanned")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", required=True,
+#                 help="Path to the image to be scanned")
+# args = vars(ap.parse_args())
 
 # load the image and compute the ratio of the old height
 # to the new height, clone it, and resize it
-image = cv2.imread(args["image"])
+#image = cv2.imread(args["image"])
+image_path = "/Users/manabchetia/Documents/PyCharm/OpenCV/document-scanner/images/page.jpg"
+image = cv2.imread(image_path)
+
 ratio = image.shape[0] / 500.0
 orig = image.copy()
-image = imutils.resize(image, height = 500)
+image = imutils.resize(image, height=500)
 
 # convert the image to grayscale, blur it, and find edges
 # in the image
@@ -38,19 +43,19 @@ cv2.destroyAllWindows()
 # find the contours in the edged image, keeping only the
 # largest ones, and initialize the screen contour
 (cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
 
 # loop over the contours
 for c in cnts:
-	# approximate the contour
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
-	# if our approximated contour has four points, then we
-	# can assume that we have found our screen
-	if len(approx) == 4:
-		screenCnt = approx
-		break
+    # if our approximated contour has four points, then we
+    # can assume that we have found our screen
+    if len(approx) == 4:
+        screenCnt = approx
+        break
 
 # show the contour (outline) of the piece of paper
 print "STEP 2: Find contours of paper"
@@ -66,11 +71,11 @@ warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
 # convert the warped image to grayscale, then threshold it
 # to give it that 'black and white' paper effect
 warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-warped = threshold_adaptive(warped, 250, offset = 10)
+warped = threshold_adaptive(warped, 250, offset=10)
 warped = warped.astype("uint8") * 255
 
 # show the original and scanned images
 print "STEP 3: Apply perspective transform"
-cv2.imshow("Original", imutils.resize(orig, height = 650))
-cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.imshow("Original", imutils.resize(orig, height=650))
+cv2.imshow("Scanned", imutils.resize(warped, height=650))
 cv2.waitKey(0)
